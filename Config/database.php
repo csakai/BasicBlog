d69d69f3cde44cbd08ab88bcff135df755e31985
csakai <cs.chris.sakai@gmail.com>
2013-09-19 04:38:33 -0400

    Posts CRUD and Tables done

---
+++ Config/database.php
@@ -0,0 +1,16 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'port' => '',
+		'host' => 'localhost',
+		'login' => 'csakai',
+		'password' => '495madNESS6174',
+		'database' => 'BasicBlog',
+		'schema' => '',
+		'prefix' => '',
+		'encoding' => 'utf8'
+	);
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+A very basic blog using CakePHP by Chris Sakai https://www.github.com/csakai
+Implemented as of 9/19/2013 4:31 AM:
+-Database Table for posts
+-ability to add, edit, delete posts
+-routing of root to posts index
+
+To be implemented:
+Comments (Add, Edit, Delete)
+Tagging (optional)
